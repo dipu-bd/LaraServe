32b1f5a2927c14372a02bec113645958edc3a7b8
dipu-bd <dipu.sudipta@gmail.com>
2017-05-02 09:58:44 +0600

    Project type enum added

---
+++ LaraServe/Models/ProjectType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaraServe.Models
+{
+    enum ProjectType
+    {
+        Unknown,
+        Laravel,
+        LaravelNode,
+        Node,
+        Meteor,
+        Sails
+    }
+}

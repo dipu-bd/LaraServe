598bd9f524d9e8ab189382d56f7c732ad8f16353
dipu-bd <dipu.sudipta@gmail.com>
2017-05-02 09:58:00 +0600

    Renamed Form1 to Main.

--- LaraServe/Main.Designer.cs
+++ LaraServe/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LaraServe
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
--- LaraServe/Program.cs
+++ LaraServe/Program.cs
@@ -16,7 +16,7 @@ namespace LaraServe
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }

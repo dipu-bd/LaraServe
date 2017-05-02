ecdbd3481c3342897abf234335f51b100f3f71e0
dipu-bd <dipu.sudipta@gmail.com>
2017-05-02 09:58:13 +0600

    Laravel model added.

---
+++ LaraServe/Models/Laravel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaraServe.Models
+{
+    class Laravel : Project
+    {
+        public Laravel()
+        {
+            this.ty
+        }
+    }
+}

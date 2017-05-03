9074bdbdeed856bcba65408bd8a14e8ef97cf368
dipu-bd <dipu.sudipta@gmail.com>
2017-05-03 06:22:26 +0600

    CueTextBox, CustomToolStrip added

---
+++ LaraServe/Components/CueTextBox.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using LaraServe.Helper;
+
+namespace LaraServe.Components
+{
+    class CueTextBox : System.Windows.Forms.TextBox
+    {
+        private string mCue;
+
+        [Localizable(true)]
+        public string Cue
+        {
+            get { return mCue; }
+            set { mCue = value; updateCue(); }
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            updateCue();
+        }
+
+        private void updateCue()
+        {
+            if (this.IsHandleCreated && mCue != null)
+            {
+                WinAPI.SendMessage(this.Handle, 0x1501, 1, mCue);
+            }
+        }
+    }
+}
---
+++ LaraServe/Components/CustomToolStrip.cs
@@ -0,0 +1,25 @@
+﻿namespace LaraServe.Components
+{
+    public class CustomToolStrip : System.Windows.Forms.ToolStrip
+    {
+        public CustomToolStrip()
+        {
+            this.Renderer = new ToolStripRenderder();
+        }
+    }
+
+    internal class ToolStripRenderder : System.Windows.Forms.ToolStripSystemRenderer
+    {
+        protected override void OnRenderToolStripBorder(System.Windows.Forms.ToolStripRenderEventArgs e)
+        {
+            // do not render border
+            //base.OnRenderToolStripBorder(e);
+        }
+
+        protected override void OnRenderButtonBackground(System.Windows.Forms.ToolStripItemRenderEventArgs e)
+        {
+            // do not render button backgroud
+            //base.OnRenderButtonBackground(e);
+        }
+    }
+}

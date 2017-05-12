4dc3b7769908404894d7d9bdc72afb80b323c8b5
dipu-bd <dipu.sudipta@gmail.com>
2017-05-13 01:50:35 +0600

    AddProject, ViewProject created

---
+++ LaraServe/ViewProject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaraServe
+{
+    public partial class ViewProject : Form
+    {
+        public ViewProject()
+        {
+            InitializeComponent();
+        }
+    }
+}
